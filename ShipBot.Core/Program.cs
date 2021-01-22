96d1ca7dc4994ff825fcced83201b421f79bfd0a
Noelwiz <dppeterson98@yahoo.com>
2021-01-21 20:07:39 -0800

    can log into bot in dev build enviorment

--- ShipBot.Core/Program.cs
+++ ShipBot.Core/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Discord;
+using Discord.WebSocket;
 
 namespace ShipBot.Core
 {
+    //https://docs.stillu.cc/guides/getting_started/first-bot.html
     /// <summary>
-
+    /// words
     /// </summary>
     class Program
     {
@@ -13,14 +16,22 @@ namespace ShipBot.Core
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             new Program().MainAsync().GetAwaiter().GetResult();
         }
-           
 
         public async Task MainAsync()
         {
+            _client = new DiscordSocketClient();
 
+            _client.Log += Log;
+            
+            var token = File.ReadAllText("./../../../bottoken.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
 
 
@@ -29,5 +40,10 @@ namespace ShipBot.Core
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
+
+
+        private DiscordSocketClient _client;
+
+
     }
 }
