033c87ddcebfb36766875af44c0866924c7076f1
Noelwiz <dppeterson98@yahoo.com>
2021-01-23 19:27:16 -0800

    coppied in annotated code for reference

--- ShipBot.Core/BotCommands.cs
+++ ShipBot.Core/BotCommands.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
@@ -61,8 +62,28 @@ namespace ShipBot.Core
             // If you do not use Dependency Injection, pass null.
             // See Dependency Injection guide for more information.
             await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
-                                            services: Ser);
+                                            services: Services);
         }
+
+        /*
+         * https://docs.stillu.cc/guides/getting_started/samples/first-bot/structure.cs
+        private async Task InitCommands()
+        {
+            // Either search the program and add all Module classes that can be found.
+            // Module classes MUST be marked 'public' or they will be ignored.
+            // You also need to pass your 'IServiceProvider' instance now,
+            // so make sure that's done before you get here.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            // Or add Modules manually if you prefer to be a little more explicit:
+            await _commands.AddModuleAsync<SomeModule>(_services);
+            // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
+
+            // Subscribe a handler to see if a message invokes a command.
+            _client.MessageReceived += HandleCommandAsync;
+        }
+        */
+
+
 
 
 
@@ -97,8 +118,64 @@ namespace ShipBot.Core
                 context: context,
                 argPos: argPos,
                 services: Services);
+
+            //todo: use this to log errors later, if they aren't already logged
+            // Uncomment the following lines if you want the bot
+            // to send a message if it failed.
+            // This does not catch errors from commands with 'RunMode.Async',
+            // subscribe a handler for '_commands.CommandExecuted' to see those.
+            //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+            //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+
         }
 
-  
+
+
+        /*
+         * https://docs.stillu.cc/guides/getting_started/samples/first-bot/structure.cs
+        private async Task HandleCommandAsync2(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasCharPrefix('!', ref pos)  || msg.HasMentionPrefix(_client.CurrentUser, ref pos) )
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(_client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await _commands.ExecuteAsync(context, pos, _services);
+
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        */
+
     }
+}
+
+
+
+
+
+
+
+
+
+
 }
--- ShipBot.Core/Program.cs
+++ ShipBot.Core/Program.cs
@@ -15,7 +15,7 @@ namespace ShipBot.Core
     {
 
         private DiscordSocketClient _client;
-        private readonly CommandService _commands;
+        private CommandService _commands;
 
         public static void Main(string[] args)
         {
@@ -25,18 +25,56 @@ namespace ShipBot.Core
 
         public async Task MainAsync()
         {
-            _client = new DiscordSocketClient();
 
-            _client.Log += Log; 
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                // How much logging do you want to see?
+                LogLevel = LogSeverity.Info,
+
+                // If you or another service needs to do anything with messages
+                // (eg. checking Reactions, checking the content of edited/deleted messages),
+                // you must set the MessageCacheSize. You may adjust the number as needed.
+                //MessageCacheSize = 50,
+
+                // If your platform doesn't have native WebSockets,
+                // add Discord.Net.Providers.WS4Net from NuGet,
+                // add the `using` at the top, and uncomment this line:
+                //WebSocketProvider = WS4NetProvider.Instance
+            });
+
+
+            //todo: figure out if this works
+            _commands = new CommandService(new CommandServiceConfig
+            {
+                // Again, log level:
+                LogLevel = LogSeverity.Info,
+
+                // There's a few more properties you can set,
+                // for example, case-insensitive commands.
+                CaseSensitiveCommands = false,
+            });
+
+            _client.Log += Log;
+            _commands.Log += Log;
+
+            //initialize commands
+            var CommandHandeler = new BotCommands(_client, _commands);
+
+            Task commandsReady = CommandHandeler.InstallCommandsAsync();
 
             var token = File.ReadAllText("./../../../bottoken.txt");
 
             await _client.LoginAsync(TokenType.Bot, token);
+
+            await commandsReady;
+
             await _client.StartAsync();
 
 
+
+
             // Block this task until the program is closed.
-            await Task.Delay(-1);
+            await Task.Delay(System.Threading.Timeout.Infinite);
 
             await _client.LogoutAsync();
         }
