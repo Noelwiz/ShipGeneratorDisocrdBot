4cb16f58b71096b7fa09ba64bc131b1ce5d0f2ee
Noelwiz <dppeterson98@yahoo.com>
2021-02-21 13:31:15 -0800

    Better Help Placeholder - related to issue #12

--- ShipBot.Core/CommandModules/HelpCommand.cs
+++ ShipBot.Core/CommandModules/HelpCommand.cs
@@ -10,10 +10,13 @@ namespace ShipBot.Core.CommandModules
     public class HelpCommand : ModuleBase<SocketCommandContext>
     {
 		// ~say hello world -> hello world
-		[Command("say")]
-		[Summary("Echoes a message.")]
-		public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
-			=> ReplyAsync(echo);
+		[Command("Help")]
+		[Summary("Gives information about commands.")]
+		public Task Help([Remainder][Summary("The text to echo")] string echo)
+        {
+            return ReplyAsync("Not yet implemented. Sorry!");
+        }
+
 
 		// ReplyAsync is a method on ModuleBase 
 	}
